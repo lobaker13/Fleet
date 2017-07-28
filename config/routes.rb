7b531c7420c25af65b5d6cdaff167d64eaa57b28
Logan <lobaker13@gmail.com>
2017-07-28 10:37:56 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,10 @@ Rails.application.routes.draw do
       resources :trips 
   end
   get 'trips/:id/search' => 'trips#search', as: :search
-
-  resources :calendars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
   devise_for :users, controllers: {
         registrations: 'users/registrations'
       }
   get 'users/:id' => 'users#show'
-  get '/redirect', to: 'calendar#redirect', as: 'redirect'
-  get '/callback', to: 'calendar#callback', as: 'callback'
 end
