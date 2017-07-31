6b48efa8e71f39e594f2da929c402bc137bb7a14
Nick Fehlinger <nfehlinger@gmail.com>
2017-07-31 00:25:17 -0400

    added ability to leave group and password protected group and trip pages, also added user search

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   #get 'trips/:id/newitin' => 'trips#newitin', as: :newitin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get '/usersearch' => 'home#usersearch', as: 'user_search'
   get '/:username' => 'home#profile', as: :profile
   devise_for :users, controllers: {
         registrations: 'users/registrations'
