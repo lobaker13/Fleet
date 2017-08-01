83cadef9173b47d4e52767708ad4b94976b5f6ee
Logan Baker <lobaker13@gmail.com>
2017-08-01 12:21:22 -0400

    awwwww how cute, the calendar follows us

---
+++ app/assets/javascripts/trips.js
@@ -0,0 +1,51 @@
+// Keeping the calendar in view while scrolling
+
+(function($) {
+
+  // Public: jScroll Plugin
+  $.fn.jScroll = function(options) {
+
+    var opts = $.extend({}, $.fn.jScroll.defaults, options);
+
+    return this.each(function() {
+      var $element = $(this);
+      var $window = $(window);
+      var locator = new location($element);
+
+      $window.scroll(function() {
+        $element
+          .stop()
+          .animate(locator.getMargin($window), opts.speed);
+      });
+    });
+
+    // Private
+    function location($element) {
+      this.min = $element.offset().top;
+      this.originalMargin = parseInt($element.css("margin-top"), 20) || 0;
+
+      this.getMargin = function($window) {
+        var max = $element.parent().height() - $element.outerHeight();
+        var margin = this.originalMargin;
+
+        if ($window.scrollTop() >= this.min)
+          margin = margin + opts.top + $window.scrollTop() - this.min;
+
+        if (margin > max)
+          margin = max;
+
+        return ({
+          "marginTop": margin + 'px'
+        });
+      }
+    }
+
+  };
+
+  // Public: Default values
+  $.fn.jScroll.defaults = {
+    speed: "slow",
+    top: 180
+  };
+
+})(jQuery);
